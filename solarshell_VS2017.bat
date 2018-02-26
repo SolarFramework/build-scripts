12b65d1fe52990b708ef968ca5d112302ccccb70
Romain Cavagna <14823759+RomArtefacto@users.noreply.github.com>
2018-02-26 14:33:30 +0100

    Update solarshell_VS2017.bat

--- solarshell_VS2017.bat
+++ solarshell_VS2017.bat
@@ -4,6 +4,6 @@ rem call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.b
 set VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\
 set VSVARPATH=%VS150COMNTOOLS:~0,-15%\VC\Auxiliary\Build
 set VSVERSION=msvc2017
-call "%VSVARPATH%\vcvarsall.bat" amd64
+call "%VSVARPATH%\vcvarsall.bat" amd64 -vcvars_ver=14.0
 cd "%CURRENTDIR%\.."
 "C:\Program Files\Git\git-bash.exe"
